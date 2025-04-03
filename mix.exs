83671ccc8bbcd79eec0f1bae148fb26e00427e01
varunrmallya <100590632+varun-r-mallya@users.noreply.github.com>
2025-04-03 11:53:45 +0530

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Envvar.MixProject do
     [
       app: :envvar,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
